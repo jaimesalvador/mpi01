78b6e7abf0d7ce08e1b9e487cc8d7f08c4ee7073
Jaime Salvador <jaime.salvador@ideasybits.com>
2023-06-21 15:10:51 +0000

    ejemplo 01

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <mpi.h>
+
+int main(int argc, char** argv ) {
+    //1. inicializar MPI
+    MPI_Init(&argc, &argv);
+
+    //2. obtener el RANK y el # de procesos
+    int rank, nprocs;
+
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);   // rank
+    MPI_Comm_size(MPI_COMM_WORLD, &nprocs); // nprocs
+
+    if(rank==0) {
+        std::printf("MPI group size: %d\n", nprocs);
+    }
+    else {
+        std::printf("Soy el rank %d\n", rank);
+    }
+
+    //std::printf("Soy el rank %d de un total de %d procesos!\n", rank, nprocs);
+
+    //3. finalizar
+    MPI_Finalize();
+
+    return 0;
+}
